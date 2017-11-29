be87c0f289b89ed26f486f78ae7766e1b21b7c25
rubenesda <rubenesda@hotmail.com>
2017-11-28 22:56:24 -0500

    Update cam.py

--- NDVI-Raspberry/cam.py
+++ NDVI-Raspberry/cam.py
@@ -47,22 +47,22 @@ buff = np.fromstring(stream.getvalue(),dtype=np.uint8)
 #Now create an OpenCV image
 image = cv2.imdecode(buff,1)
 
-##dist = np.array([0.177862857743418, -0.254303591272533,  0.005644936356933, 0.001247279996218, 0.000000000000000])
-##mtx = np.matrix([[2511.826513697280000, 0, 1676.903161780660600],[0, 2695.418467746039800, 1239.544777325847000],[0,0, 1]])
-##
-##h,  w = image.shape[:2]
-##newcameramtx, roi=cv2.getOptimalNewCameraMatrix(mtx,dist,(w,h),1,(w,h))
-##
-### undistort
-##dst = cv2.undistort(image, mtx, dist, None, newcameramtx)
-##
-### crop the image
-##x,y,w,h = roi
-##dst = dst[y:y+h, x:x+w]
-##
-###write text
-##font = cv2.FONT_HERSHEY_SIMPLEX
-##cv2.putText(dst, 'shutter: '+str(speed)+', ISO: 100, exposure mode: off.', (10,25),font,1,(255,255,255),2)
+dist = np.array([0.177862857743418, -0.254303591272533,  0.005644936356933, 0.001247279996218, 0.000000000000000])
+mtx = np.matrix([[2511.826513697280000, 0, 1676.903161780660600],[0, 2695.418467746039800, 1239.544777325847000],[0,0, 1]])
+
+h,  w = image.shape[:2]
+newcameramtx, roi=cv2.getOptimalNewCameraMatrix(mtx,dist,(w,h),1,(w,h))
+
+# undistort
+dst = cv2.undistort(image, mtx, dist, None, newcameramtx)
+
+# crop the image
+x,y,w,h = roi
+dst = dst[y:y+h, x:x+w]
+
+#write text
+font = cv2.FONT_HERSHEY_SIMPLEX
+cv2.putText(dst, 'shutter: '+str(speed)+', ISO: 100, exposure mode: off.', (10,25),font,1,(255,255,255),2)
 dst = image
 
 if(cuenta==1):
@@ -74,7 +74,7 @@ if(cuenta==3):
 if(cuenta==4):
     cv2.imwrite(str(cuenta)+filename+'.jpg',dst)
     cuenta = 0
-#cuenta = cuenta + 1
+cuenta = cuenta + 1
 
 file = open("conteo.txt","w")
 file.write("Cuenta"+"\n"+str(cuenta))
