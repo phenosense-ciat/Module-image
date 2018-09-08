2d1a0fc3a979d36c1206088ee2d3d9ecb2fcd8bb
rubenesda <rubenesda@hotmail.com>
2018-09-07 22:47:42 -0500

    Update runCamEventos.py

--- NDVI-Raspberry/Station/runCamEventos.py
+++ NDVI-Raspberry/Station/runCamEventos.py
@@ -1,53 +1,63 @@
+#!/usr/bin/python
+from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor
+
 import RPi.GPIO as GPIO
 import time
 import os
+import atexit
 nota = "sudo shutdown now"
+
+mh = Adafruit_MotorHAT(addr=0x60)
+
+def turnOffMotors():
+	mh.getMotor(1).run(Adafruit_MotorHAT.RELEASE)
+
+atexit.register(turnOffMotors)
+
+myMotor = mh.getMotor(1)
+
+myMotor.setSpeed(255)
+myMotor.run(Adafruit_MotorHAT.FORWARD);
+
+myMotor.run(Adafruit_MotorHAT.RELEASE);
 
 #Adjust for where your switch is connected
 buttonPin1 = 17
 buttonEnd1 = 27
 buttonEnd2 = 22
 #Move motor wrist
-pinLED = 18
-pinLED2 = 24
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(buttonPin1,GPIO.IN, GPIO.PUD_UP)
 GPIO.setup(buttonEnd1,GPIO.IN, GPIO.PUD_UP)
 GPIO.setup(buttonEnd2,GPIO.IN, GPIO.PUD_UP)
-GPIO.setup(pinLED,GPIO.OUT)
-GPIO.setup(pinLED2,GPIO.OUT)
 
 GPIO.setwarnings(False)
 
-#Adjust PWM for motor wrist
-number = 100
-#adjust velocity
-velocity = 100
-#p spin a side and p1 spin in the other side
-p = GPIO.PWM(pinLED,number)
-p1 = GPIO.PWM(pinLED2,number)
-
 def my_foto(channel):
-    p.stop()
+    myMotor.run(Adafruit_MotorHAT.RELEASE)
     # Uncomment this line of code and comment on the following if you are working with a virtual environment. Otherwise do not take any action.
     #os.system("/home/pi/.virtualenvs/cv/bin/python cam.py")
     os.system("sudo python cam.py")
-    p.start(velocity)
+    myMotor.run(Adafruit_MotorHAT.FORWARD)
     #time.sleep(0.3)
 
 GPIO.add_event_detect(buttonPin1, GPIO.FALLING, callback=my_foto, bouncetime=200)
 
 try:
-    p.start(velocity)
+    print("begin")
+    myMotor.run(Adafruit_MotorHAT.FORWARD)
     GPIO.wait_for_edge(buttonEnd1, GPIO.FALLING)
-    p.stop()
+    print("stop")
+    myMotor.run(Adafruit_MotorHAT.RELEASE)
     #Point control of flag 1
     #print("flag 1")
     print("Returning")
-    p1.start(velocity)
+    myMotor.run(Adafruit_MotorHAT.BACKWARD)
     GPIO.remove_event_detect(buttonPin1)
     GPIO.wait_for_edge(buttonEnd2, GPIO.FALLING)
-    p1.stop()
+    print("Stop")
+    myMotor.run(Adafruit_MotorHAT.RELEASE)
     #Point control flag 2
     #print("Flag 2")
     for x in range (1,5):
